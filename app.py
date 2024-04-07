0e0d32cb8cfd76c13a49731de709d5b74d949a67
MartinFernandezSantia <martin-santia@hotmail.com.ar>
2024-04-07 01:08:30 -0300

    Finalizado el frontend del formulario

--- app.py
+++ app.py
@@ -2,100 +2,98 @@ import sys
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import QDateTime
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        
-        self.setWindowTitle("My App")
+
+        self.setWindowTitle("Gestor de gastos")
+        self.resize(300, 200)
 
         # Layout principal
-        layout = QVBoxLayout()        
-        layout.setSpacing(20)
+        self.layout = QVBoxLayout()
+        self.layout.setSpacing(20)
 
         # Primera fila
-        layout2 = QVBoxLayout()
-        layout2.setSpacing(0)
-
         self.nombre_transaccion = QComboBox()
         self.nombre_transaccion.addItems(["", "Opcion 2", "Opcion 3"])
         self.nombre_transaccion.setEditable(True)
+        self.labeled_widget(
+            "Nombre de la transacción", self.nombre_transaccion, self.layout
+        )
 
-        self.nombre_transaccion_label = QLabel("Nombre de la transacción")
-
-        layout2.addWidget(self.nombre_transaccion_label)
-        layout2.addWidget(self.nombre_transaccion)
-        
         # Segunda fila
-        layout3 = QVBoxLayout()
-        layout4 = QVBoxLayout()
-        layout5 = QHBoxLayout()
-
-        layout3.setSpacing(0)
-        layout4.setSpacing(0)
-
         self.cant_dinero = QDoubleSpinBox()
         self.cant_dinero.setRange(0, 999999999.99)
+        self.labeled_widget("Cantidad de dinero", self.cant_dinero, self.layout)
 
-        self.cant_dinero_label = QLabel("Cantidad de dinero")
+        # Tercera fila
+        self.layout2 = QHBoxLayout()
+
+        self.cantidad = QSpinBox()
+        self.cantidad.setRange(1, 999999)
+        self.labeled_widget("Cantidad", self.cantidad, self.layout2)
 
         self.moneda = QComboBox()
         self.moneda.addItems(["AR$", "U$D"])
+        self.labeled_widget("Moneda", self.moneda, self.layout2)
 
-        self.moneda_label = QLabel("Moneda")
+        self.layout.addLayout(self.layout2)
 
-        layout3.addWidget(self.cant_dinero_label)
-        layout3.addWidget(self.cant_dinero)
-
-        layout4.addWidget(self.moneda_label)
-        layout4.addWidget(self.moneda)
-
-        layout5.addLayout(layout3)
-        layout5.addLayout(layout4)
-
-        # Tercera fila
-        layout6 = QVBoxLayout()
-        layout6.setSpacing(0)
-
-        self.cantidad_label = QLabel("Cantidad")
-        self.cantidad = QSpinBox()
-        self.cantidad.setRange(1, 999999)
-
-        layout6.addWidget(self.cantidad_label)
-        layout6.addWidget(self.cantidad)
-
-        # Cuarta fila
-        layout7 = QVBoxLayout()
-        layout7.setSpacing(0)
-
-        self.fecha_hora_label = QLabel("Fecha y hora")
+        # # Cuarta fila
         self.fecha_hora = QDateTimeEdit(self, calendarPopup=True)
         self.fecha_hora.setDateTime(QDateTime.currentDateTime())
         self.fecha_hora.setDisplayFormat("dd/MM/yyyy | hh:mm AP")
+        self.labeled_widget("Fecha y hora", self.fecha_hora, self.layout)
 
-        layout7.addWidget(self.fecha_hora_label)
-        layout7.addWidget(self.fecha_hora)
+        # # Quinta fila
+        self.categoria = QComboBox()
+        self.categoria.addItems(["Otros", "Servicios", "Tecnología", "Alimentación"])
+        self.labeled_widget("Categoría", self.categoria, self.layout)
 
-        # Quinta fila
+        # # Sexta fila
+        self.metodo_pago = QComboBox()
+        self.metodo_pago.addItems(["Efectivo", "Debito", "Credito"])
+        self.labeled_widget("Metodo de pago", self.metodo_pago, self.layout)
 
-        # Ultima fila
-        button = QPushButton("Print")
-        button.clicked.connect(self.print_stuff)
+        # # Ultima fila
+        buttons_layout = QVBoxLayout()
+        buttons_layout.setSpacing(3)
 
-        # Asignacion de widgets y filas a layout principal
-        layout.addLayout(layout2)
-        layout.addLayout(layout5)
-        layout.addLayout(layout6)
-        layout.addLayout(layout7)
-        layout.addWidget(button)
+        button1 = QPushButton("+")
+        button2 = QPushButton("-")
+        button1.setStyleSheet(
+            "background-color: #116233; font-size: 15px; color: white;"
+        )
+        button1.font()
+        button2.setStyleSheet(
+            "background-color: #841717; font-size: 15px; color: white;"
+        )
+
+        buttons_layout.addWidget(button1)
+        buttons_layout.addWidget(button2)
+
+        self.layout.addLayout(buttons_layout)
+        # button.clicked.connect(self.print_stuff)
 
         widget = QWidget()
-        widget.setLayout(layout)
+        widget.setLayout(self.layout)
         self.setCentralWidget(widget)
 
-    def print_stuff(self):
-        print(self.nombre_transaccion_input.currentText())
+    def labeled_widget(self, label_text, widget, parent_layout):
+        new_layout = QVBoxLayout()
+        new_layout.setSpacing(0)
+
+        label = QLabel(label_text)
+
+        new_layout.addWidget(label)
+        new_layout.addWidget(widget)
+
+        parent_layout.addLayout(new_layout)
+
 
 app = QApplication(sys.argv)
+app.setStyle("fusion")
 
 window = MainWindow()
 window.show()
